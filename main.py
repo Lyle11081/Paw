bc67579046c6c09a59bc2f1a551112314f171b71
Lyle11081 <lyle2539@gmail.com>
2026-04-21 15:49:24 +0100

    V2

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from calculateBMI import *
+from ConvertFtoC import *
 #BMi or C to F
 choice = input("Please select an option BMi or Convert : ")
-#if choice == "BMi":
-#if choice == "convert":
+if choice == "bmi":
+    weight = float(input("Enter your weight in kg : "))
+    height = float(input("Enter heightin meters : "))
+    print(calculateBMI(height,weight))
+elif choice == "convert":
+    fahrenheitValue = input("Enter your Farenheight")
+    print(FtoCconvertor(fahrenheitValue))
+else:
+      print("Invalid")
+
